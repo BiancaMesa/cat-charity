14ca0485f67d914e03ee7a99c24b57e4c787ba60
BiancaMesa <bianca.koraa@gmail.com>
2024-04-08 21:33:46 +0200

    support section

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -4,7 +4,12 @@ function Support() {
   return (
     <section className="supportContainer">
         <h1 className="supportContainer__title">Support</h1>
-        <p className="supportContainer__description"></p>
+        <div className="supportContainer__titleBottomLine"></div>
+        <h3 className="supportContainer__subtitle">This is how you can contribute</h3>
+        <p className="supportContainer__description">Cat purring lap thread dangling curl play hide and seek magic protection good energy kitten crystals sleep angels good heart furry babies. 
+        We are so thankful for all of the beautiful souls who donate because thanks to them these angels can have a better quality of life.
+        Small amounts make a great difference.</p>
+        <button className="supportContainer__patreonButton">Patreon</button>
     </section>
   );
 }
