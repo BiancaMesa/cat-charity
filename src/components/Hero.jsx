fe37ddfd7396ac3e5c1054485acdf74d9a081f43
BiancaMesa <bianca.koraa@gmail.com>
2024-05-16 21:28:18 +0200

    donate button added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,7 @@ function Hero() {
   return (
     <section className="heroContainer">
         <h1 className="heroContainer__title">Want to save some angels?</h1>
+        <button className="heroContainer__donateButton">DONATE</button>
         <img className="heroContainer__heroImage" src={hero}/>
     </section>
   );
